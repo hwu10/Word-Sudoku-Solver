43dec7cdbfd0fbf544283d8de620dc2c09061026
Bryan Hwu <hwu10@illinois.edu>
2016-10-22 12:35:51 -0500

    Patched up doesWordFit

--- word_sudoku.py
+++ word_sudoku.py
@@ -1,31 +1,46 @@
+#current problem is we cant find a fit for 'outraged' after placing 'marveling'
+#so essentially our backtracking is not working
+
+count = 1
+
 def init():
-    mazeMatrix = [] #mazeMatrix is [y][x] i.e. mazeMatrix[3][0] gives us (0, 3)
+    puzzle = [] #mazeMatrix is [y][x] i.e. mazeMatrix[3][0] gives us (0, 3)
     startingLetters = []
     words = []
     stateStack = []
-    with open('gridtest.txt') as mazeText:
+    with open('grid1.txt') as mazeText:
         y = 0
         for line in mazeText:
+            line = line.lower()
             line = line.strip()
             mazeLine = []
             for x in range(0,len(line)):
                 mazeLine.append(line[x])
                 if(line[x]!="_"):
-                    letter = [x, y, line[x]]
+                    letter = [x, y, line[x].lower()]
                     startingLetters.append(letter)
-            mazeMatrix.append(line)
+            puzzle.append(mazeLine)
             y+=1
 
-    with open('bank1.txt') as wordText:
+    with open('bank2.txt') as wordText:
         for line in wordText:
+            line = line.lower()
             line = line.rstrip()
-            word = Word(len(line), line.lower())
+            word = Word(len(line), line.lower(), startingLetters)
             word.domain = pruneDomain(word)
             words.append(word)
        
+    #print(sudoku_solver(mazeMatrix, words, stateStack, 0))
+    printSudoku(sudoku_solver(puzzle, words, stateStack, 0))
 
 def sudoku_solver(puzzle, words, stack, err):
+    printSudoku(puzzle)
+    #print()
+    #print('func count: ' + str(count))
+    global count
+    count += 1
     if err == -2:
+        print('fucked')
         return -2
     if err == -1:
         prevState = stack.pop()
@@ -56,15 +71,20 @@ def sudoku_solver(puzzle, words, stack, err):
         nextWord = wordTuple[0]
         nextWordIndex = wordTuple[1]
         #Find the word's next valid value and its index
+        print(nextWord.word)
+        printDomain(words[nextWordIndex].domain)
         valueTuple = findWordFit(puzzle, wordTuple[0])
+        if (valueTuple == False):
+            err = -1
+            return sudoku_solver(puzzle, words, stack, err)
         valueFound = valueTuple[0]
         #Save the state of the game and push it onto the state stack
         currState = State(words, puzzle)
         stack.append(currState)
         #Insert word into Sudoku
         puzzle = insertWord(puzzle, nextWord.word, valueFound)
-        word[nextWordIndex].placed = True
-        word[nextWordIndex].domain = [valueFound]
+        words[nextWordIndex].placed = True
+        words[nextWordIndex].domain = [valueFound]
         for i in range(0, len(words)):  
             if (words[i].word != nextWord.word):
                 words[i].domain = pruneDomain(words[i]) 
@@ -86,12 +106,21 @@ class Word:
     word = []
     placed = None
 
-    def __init__(self, length, word):
+    def __init__(self, length, word, startingLetters):
         self.word = word.strip()
         self.size = length
-        self.domain = createDomain(self.word[0])
+        self.domain = createDomain(self.word[0], startingLetters)
         self.placed = False
 
+def printDomain(domain):
+    i = 0;
+    for value in domain:
+        print (value, end= " ")
+        if(i%4 == 0):
+            print()
+        i += 1
+    print()
+     
 def checkIfFilled(puzzle):
     for y in range(0,9):
         for x in range(0,9):
@@ -100,7 +129,7 @@ def checkIfFilled(puzzle):
     return True
 
 
-def createDomain(start_letter):
+def createDomain(start_letter, startingLetters):
     orientations = ['H', 'V']
     domain = []
     temp = []
@@ -129,14 +158,14 @@ def pruneDomain(word):
     return word.domain
 
 def insertWord(puzzle, word, value):
-    if value[2] == 'H'
+    if value[2] == 'H':
         insertx = value[0]
         inserty = value[1]
         for char in word:
             puzzle[insertx][inserty] = char
             insertx += 1
 
-    if value[2] == 'V'
+    if value[2] == 'V':
         insertx = value[0]
         inserty = value[1]
         for char in word:
@@ -151,7 +180,6 @@ def getNextVariable(words):
     words.sort(key= lambda x: x.size, reverse=True)
     maxLength = 0
     longestWord = None
-    count = 0
     #Words are sorted in descending order of length
     for word in words:
         #First check if the domain has less than four possible values
@@ -168,21 +196,20 @@ def getNextVariable(words):
         elif word.size == maxLength:
             if len(word.domain) > len(longestWord.domain):
                 longestWord = word
-        count += 1
-    return (longestWord, count)
+    return (longestWord, words.index(longestWord))
 
 #Given word and possible word starting point,
 #check whether location is valid and if the word
 #results in illegal duplicates in rows, columns, and 3x3 boxes
-def doesWordFit(sudoku, word, domain):
+def doesWordFit(puzzle, word, domain):
     count = 0
     if domain[2] == 'H':
         #Ensure no duplicate letters in horizontal row and if word fits
         for x in range(0, 9):
             if x in range(domain[0], domain[0]+word.size):
-                if sudoku[domain[1]][x] != '_' and sudoku[domain[1]][x] != word.word[count]:
+                if puzzle[x][domain[1]] != '_' and puzzle[x][domain[1]] != word.word[count]:
                     return False
-            if sudoku[domain[1]][x] in list(word.word):
+            if puzzle[x][domain[1]] in list(word.word):
                 return False
         count += 1
         #Check 3x3 box that each char in the word is in
@@ -191,7 +218,7 @@ def doesWordFit(sudoku, word, domain):
         for char in word.word:
             for x in range(charx - charx%3, charx - charx%3 + 3):
                 for y in range(chary - chary%3, chary - chary%3 + 3):
-                    if sudoku[x][y] == char:
+                    if puzzle[x][y] == char:
                         return False
             charx += 1
 
@@ -199,9 +226,9 @@ def doesWordFit(sudoku, word, domain):
         #Ensure no duplicate letters in vertical row and if word fits
         for y in range(0, 9):
             if y in range(domain[0], domain[0]+word.size):
-                if sudoku[domain[1]][y] != '_' and sudoku[domain[1]][y] != word.word[count]:
+                if puzzle[domain[0]][y] != '_' and puzzle[domain[0]][y] != word.word[count]:
                     return False
-            if sudoku[domain[1]][y] in list(word.word):
+            if puzzle[domain[0]][y] in list(word.word):
                 return False
         count += 1
         #Check 3x3 box that each char in the word is in
@@ -210,7 +237,7 @@ def doesWordFit(sudoku, word, domain):
         for char in word.word:
             for x in range(charx - charx%3, charx - charx%3 +3):
                 for y in range(chary - chary%3, chary - chary%3 +3):
-                    if sudoku[x][y] == char:
+                    if puzzle[x][y] == char:
                         return False
             chary += 1
     return True
@@ -222,5 +249,11 @@ def findWordFit(puzzle, word):
             return (value, index)
         index += 1
     return False
+
+def printSudoku(puzzle):
+    for y in range(0, 9):
+        for x in range(0, 9):
+            print(puzzle[x][y], end=" ")
+        print()
 
 init()
